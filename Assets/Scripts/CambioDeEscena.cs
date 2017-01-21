ef0d4fe8e842b093439f14131f3bd65a1ef7ac1a
Lauren <lau.16.9@gmail.com>
2017-01-21 12:17:19 +0100

    Añadido cambio de escenas y gameplay de prueba.

---
+++ Assets/Scripts/CambioDeEscena.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioDeEscena : MonoBehaviour
+{
+    public static CambioDeEscena instancia { get; private set; }
+
+    Scene escenaActual;
+    int indiceNivel;
+
+    private void Awake()
+    {
+        instancia = this;
+        indiceNivel = 1;
+
+        SceneManager.LoadScene(indiceNivel, LoadSceneMode.Additive);
+    }
+
+    public void SiguienteNivel()
+    {
+        SceneManager.UnloadSceneAsync(indiceNivel);
+        indiceNivel++;
+        SceneManager.LoadScene(indiceNivel, LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/GizmoCubo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GizmoCubo : MonoBehaviour
+{
+    public Color color;
+
+    void OnDrawGizmos()
+    {
+        Matrix4x4 rotationMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+        Gizmos.matrix = rotationMatrix;
+
+        Gizmos.color = color;
+        Gizmos.DrawCube(Vector3.zero, new Vector3(1, 1, 1));
+    }
+}
